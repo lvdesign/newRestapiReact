5c1f9faa495c1c666b31c14c17fd251a73b778ab
lvdesign <mailbox@lvdesign.com.fr>
2018-12-07 14:55:32 +0100

    Basic auth

--- beta-react-Mongodb/reactRESTapi-base/app.js
+++ beta-react-Mongodb/reactRESTapi-base/app.js
@@ -36,12 +36,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
+// autentification base
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+//
 // ROUTER ---- pour login
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use(express.static(path.join(__dirname, 'public')));
-//
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- beta-react-Mongodb/reactRESTapi-base/routes/users.js
+++ beta-react-Mongodb/reactRESTapi-base/routes/users.js
@@ -2,16 +2,30 @@ var express = require('express');
 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
+var User = require('../models/user');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
 
+router.get('/',(req, res, next) => {
+  User.find({})
+      .then( (users) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(users);
+      })
+        .catch((err) => {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+        });
+});
 
 module.exports = router;
